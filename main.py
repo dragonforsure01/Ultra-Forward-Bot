f39d9f3b1010848bf62ca687460b12feb335de90
dragonforsure01 <dragonforsure01@gmail.com>
2026-01-03 16:25:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+  
 # Jishu Developer 
 # Don't Remove Credit 🥺
 # Telegram Channel @Madflix_Bots
